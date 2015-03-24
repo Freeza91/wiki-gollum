46e24403ae2cca6a47930eb151f2917e7df9e299
rudy <347212291@qq.com>
2015-03-24 15:19:45 +0800

    add gollum wiki CRUD

---
+++ app/controllers/wikis_controller.rb
@@ -0,0 +1,64 @@
+class WikisController < ApplicationController
+
+  def index
+    @wikis = Wiki::DATA.pages
+  end
+
+  def new
+    @wiki = Wiki.new(name: 'hello hdfshifsdhisdfhisdf')
+  end
+
+  def show
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    commit = {
+      name: 'rudy',
+      message: 'commit agagin ',
+      email: '347212291@qq.com'
+    }
+    if @wiki.update_attributes(params[:wiki], commit)
+      redirect_to wiki_path(@wiki.name)
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @wiki = Wiki.new(params[:wiki])
+    commit = {
+      name: 'rudy',
+      message: 'commit agagin ',
+      email: '347212291@qq.com'
+    }
+    begin
+      @wiki.save(commit)
+      redirect_to wiki_path(@wiki.name)
+    rescue Gollum::DuplicatePageError => e
+      render :text => "Duplicate page: #{e.message}"
+    end
+  end
+
+  def delete
+    @wiki = Wiki.find(params[:id])
+    commit = {
+      name: 'rudy',
+      message: 'commit agagin ',
+      email: '347212291@qq.com'
+    }
+    @wiki.delete_page(@wiki.page, commit)
+    redirect_to wikis_path
+  end
+
+  def preview
+    @body = params[:body]
+    @content = Wiki::DATA.preview_page("Preview", @body, :markdown).formatted_data
+
+    respond_to do |format|
+      format.json
+    end
+  end
+
+end
